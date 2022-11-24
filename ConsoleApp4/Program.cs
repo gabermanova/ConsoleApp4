891ffba0aa584095308318b9ee5962e72bb4702f
Екатерина_Габерманов <_@DESKTOP-M91PJSA>
2022-11-24 23:29:20 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ConsoleWhile
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a = 0, e = 0.0001, q = 0, s = 0, x = 0.12;
+            int n = 0;
+            a = x; // Инициализация цикла
+            s = a;
+            while (Math.Abs(a) > e) // Цикл
+            {
+                q = (x / (n + 1) * (n + 2)) * Math.Cos((Math.PI / 4) * n);
+                a *= q;
+                s += a;
+                n++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Результат");
+            Console.WriteLine();
+            Console.WriteLine("Сумма s=" + Convert.ToString(s));
+            Console.WriteLine("Число членов ряда n=" + Convert.ToString(n));
+            Console.WriteLine();
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
